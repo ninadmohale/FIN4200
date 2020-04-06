fa27df8b76bf9c57190e3c48dacbcd12efb45001
ninadmohale <nrmohale@mtu.edu>
2020-04-06 14:13:53 -0400

    Added the greeks

--- BSM_oop.py
+++ BSM_oop.py
@@ -16,15 +16,15 @@ class option:
 
         def d1_calc(S, K, r, vol, T, t):
             # Calculates d1 in the BSM equation
-            return (np.log(S/K) + (r + 0.5 * vol**2)*(T-t))/(vol*np.sqrt(T-t))
+            return ((np.log(S/K) + (r + 0.5 * vol**2)*(T-t))/(vol*np.sqrt(T-t)))
 
         def BS_call(S, K, r, vol, T, t, d1, d2):
-            return S * norm.cdf(d1) - K * np.exp(-r*T)*norm.cdf(d2)
+            return (S * norm.cdf(d1) - K * np.exp(-r*T)*norm.cdf(d2))
 
         def BS_put(S, K, r, vol, T, t, d1, d2):
-            return BS_call(S, K, r, vol, T, t, d1, d2) - S + np.exp(-r*(T-t))*K
+            return (BS_call(S, K, r, vol, T, t, d1, d2) - S + np.exp(-r*(T-t))*K)
 
-        self.d1 = (np.log(S/K) + (r + 0.5 * vol**2)*(T-t))/(vol*np.sqrt(T-t))
+        self.d1 = d1_calc(S, K, r, vol, T, t)
         self.d2 = self.d1 - vol * np.sqrt(T)
 
         if otype == 'call':
@@ -32,25 +32,66 @@ class option:
         else:
             self.price = BS_put(S, K, r, vol, T, t,self.d1,self.d2)
 
+        # Greeks
+        def delta(S, K, r, vol, T, t, otype, d1, d2):
+            if(otype == "call"):
+                delta = np.exp(-(T-t))*norm.cdf(d1)
+            elif(otype == "put"):
+                delta = -np.exp(-(T-t))*norm.cdf(-d1)
+            return delta
+
+        def vega(S, K, r, vol, T, t, otype, d1, d2):
+            return S * norm.pdf(d1) * np.sqrt(T-t)
+
+        def rho(S, K, r, vol, T, t, otype, d1, d2):
+            if(otype == "call"):
+                rho = K*(T-t)*np.exp(-r*(T-t))*norm.cdf(d2)
+            elif(otype == "put"):
+                rho = -K*(T-t)*np.exp(-r*(T-t))*norm.cdf(-d2)
+            return rho
+
+        def theta(S, K, r, vol, T, t, otype, d1, d2):
+            if(otype == "call"):
+                theta = -(S*norm.pdf(d1)*vol / (2*np.sqrt(T-t))) - r*K*np.exp(-r*(T-t))*norm.cdf(d2)
+            elif(otype == "put"):
+                theta = -(S*norm.pdf(d1)*vol / (2*np.sqrt(T-t))) + r*K*np.exp(-r*(T-t))*norm.cdf(-d2)
+            return theta
+
+        def gamma(S, K, r, vol, T, t, otype, d1, d2):
+            gamma = (norm.pdf(d1)) / (S * vol * np.sqrt(T-t))
+            return gamma
+
+        def charm(S, K, r, vol, T, t, otype, d1, d2):
+            charm = -norm.pdf(d1)*(2*r*(T-t) - d2*vol*np.sqrt(T-t))/(2*(T-t)*vol*np.sqrt(T-t))
+            return charm
+
+        self.delta = delta(S, K, r, vol, T, t, otype, self.d1, self.d2)
+        self.vega = vega(S, K, r, vol, T, t, otype, self.d1, self.d2)
+        self.rho = rho(S, K, r, vol, T, t, otype, self.d1, self.d2)
+        self.theta = theta(S, K, r, vol, T, t, otype, self.d1, self.d2)
+        self.gamma = gamma(S, K, r, vol, T, t, otype, self.d1, self.d2)
+        self.charm = charm(S, K, r, vol, T, t, otype, self.d1, self.d2)
 
 
+a1 = option('call',254,250,0.1,0.2,10,0)
 
+print(type(a1))
 
-a1 = option('call',254,250,10,2,3,0)
+print('type',a1.otype)
+print('S',a1.S)
+print('K',a1.K)
+print('vol',a1.vol)
+print('r',a1.r)
+print('T',a1.T)
+print('t',a1.t)
+print('d1',a1.d1)
+print('d2',a1.d2)
 
-print(a1)
+print('price',a1.price)
 
-
-print(a1.otype)
-print(a1.S)
-print(a1.K)
-print(a1.vol)
-print(a1.r)
-print(a1.T)
-print(a1.t)
-print(a1.d1)
-print(a1.d2)
-
-print(a1.price)
-
-# (otype,S,K,vol,r,T,t)
+print('delta',a1.delta)
+print('vega',a1.vega)
+print('rho',a1.rho)
+print('theta',a1.theta)
+print('gamma',a1.gamma)
+print('charm',a1.charm)
